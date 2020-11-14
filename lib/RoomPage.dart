3cc07c841c658d90dd2567c1e1abfeff772d2d51
onima-t <maphys6241@gmail.com>
2020-11-15 01:56:31 +0900

    fix tab function

--- lib/RoomPage.dart
+++ lib/RoomPage.dart
@@ -24,41 +24,13 @@ class _RoomPageState extends State<RoomPage> {
 
   @override
   Widget build(BuildContext context) {
-    Column make_column({String image_name, String user_name}) => Column(
-      children: [
-        Container(
-          width: 200.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.contain,
-              image: AssetImage(image_name),
-            ),
-          ),
-        ),
-        Text(user_name)
-      ],
-    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Room#51'),
         backgroundColor: ABColor,
       ),
       backgroundColor: BGColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              make_column(image_name: 'images/twitter.png',user_name: 'Aさん'),
-              make_column(image_name: 'images/monst.png',user_name: 'Bさん'),
-            ],
-          ),
-          Column(
-            children: [],
-          )
-        ],
-      ),
+      body: _pageList[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -76,85 +48,3 @@ class _RoomPageState extends State<RoomPage> {
     );
   }
 }
-
-class CustomPage extends StatelessWidget {
-  final Color pannelColor;
-  final String title;
-
-  CustomPage({@required this.pannelColor, @required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    final titleTextStyle = Theme.of(context).textTheme.title;
-    return Container(
-      child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-              color: pannelColor,
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: titleTextStyle.fontSize,
-                color: titleTextStyle.color,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RoomTab extends StatefulWidget {
-  @override
-  _RoomTabState createState() => _RoomTabState();
-}
-
-class _RoomTabState extends State<RoomTab> {
-  Container make_icons(String asset_name) => Container(
-      width: 200.0,
-      height: 200.0,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('$asset_name'),
-          ),
-          border: Border.all(width: 20,color: Colors.indigo)
-      )
-  );
-  @override
-  Widget build(BuildContext context) {
-    return  Center(
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 300.0,
-            child: ListView(scrollDirection: Axis.horizontal, children: [
-              make_icons('images/human1.png'),
-              make_icons('images/dog.png'),
-              make_icons('images/cat.png'),
-              make_icons('images/clown-fish.png'),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              'ルーム',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
--- lib/Roomtab.dart
+++ lib/Roomtab.dart
@@ -6,45 +6,33 @@ class RoomTab extends StatefulWidget {
 }
 
 class _RoomTabState extends State<RoomTab> {
-  Container make_icons(String asset_name) => Container(
-      width: 200.0,
-      height: 200.0,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
+  Column make_column({String image_name, String user_name}) => Column(
+    children: [
+      Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
           image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('$asset_name'),
+            fit: BoxFit.contain,
+            image: AssetImage(image_name),
           ),
-          border: Border.all(width: 20,color: Colors.indigo)
-      )
+        ),
+      ),
+      Text(user_name)
+    ],
   );
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 300.0,
-            child: ListView(scrollDirection: Axis.horizontal, children: [
-              make_icons('images/human1.png'),
-              make_icons('images/dog.png'),
-              make_icons('images/cat.png'),
-              make_icons('images/clown-fish.png'),
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              'ルーム',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          )
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            make_column(image_name: 'images/twitter.png',user_name: 'Aさん'),
+            make_column(image_name: 'images/monst.png',user_name: 'Bさん')
+          ],
+        ),
+      ],
     );
   }
 }
