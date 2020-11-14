1c9508314c03dfc60ff8eeafa77ca5789845359e
onima-t <maphys6241@gmail.com>
2020-11-14 18:47:32 +0900

    main を元に戻しました

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,129 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dice'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber=Random().nextInt(6) + 1;
-  int rightDiceNumber=Random().nextInt(6) + 1;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: FlatButton(
-                  onPressed: (){
-                    setState(() {
-                      leftDiceNumber = Random().nextInt(6) + 1;
-                      print('diceNumber = $leftDiceNumber');
-                    });
-                  },
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('images/dice$leftDiceNumber.png'),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: FlatButton(
-                  onPressed: (){
-                    setState(() {
-                      rightDiceNumber = Random().nextInt(6) + 1;
-                      print(rightDiceNumber);
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(
-                      image: AssetImage('images/dice$rightDiceNumber.png'),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          FlatButton(
+    // final size = MediaQuery.of(context).size;
+    // final padding = MediaQuery.of(context).padding;
+    // var maxHeight = size.height - padding.top - padding.bottom;
+    return MaterialApp(
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('mi card'),
+        // ),
+        backgroundColor: Colors.indigo[500],
+        body: Center(
+          child: RaisedButton(
+            child: Text('自習室へいく！'),
             onPressed: (){
-              setState(() {
-                leftDiceNumber=Random().nextInt(6) + 1;
-                rightDiceNumber=Random().nextInt(6) + 1;
-              });
+              // Navigator.push(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => NextPage(),
+              //     )
+              // ),
+              // 押したら反応するコードを書く
+              // 画面遷移のコード
             },
-            child: Text('Roll dices'),
-            color: Colors.white,
-          )
-        ],
+          ),
+        ),
+        // body: SafeArea(
+        //   child: Row(
+        //     // crossAxisAlignment: CrossAxisAlignment.start,
+        //     // 交差軸方向に引き伸ばして配置
+        //     // crossAxisAlignment: CrossAxisAlignment.stretch,
+        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //     // verticalDirection: VerticalDirection.down,
+        //     children: <Widget>[
+        //       Column(
+        //         mainAxisSize: MainAxisSize.min,
+        //         // crossAxisAlignment: CrossAxisAlignment.stretch,
+        //         children: <Widget>[
+        //           Container(
+        //             width: 100.0,
+        //             height: 645.0,
+        //             color: Colors.indigo,
+        //             // child: Text('2'),
+        //           ),
+        //         ],
+        //       ),
+        //       Column(
+        //         mainAxisSize: MainAxisSize.min,
+        //         children: <Widget>[
+        //           Container(
+        //             width: 80.0,
+        //             height: 35.0,
+        //             color: Colors.red[400],
+        //             child: Text('Creat Room\n or Share\n',
+        //             ),
+        //           ),
+        //           Container(
+        //             width: 100.0,
+        //             height: 40.0,
+        //             color: Colors.indigo,
+        //             ),
+        //           CircleAvatar(
+        //             radius: 50.0,
+        //             backgroundColor: Colors.indigo[400],
+        //             backgroundImage: AssetImage('images/Ree.png'),
+        //           ),
+        //           Text(
+        //             'Study Room',
+        //             style: TextStyle(
+        //               fontFamily: 'Pacifico',
+        //               fontSize: 30.0,
+        //               fontWeight: FontWeight.bold,
+        //               color: Colors.white,
+        //             ),
+        //           ),
+        //           Container(
+        //             width: 100.0,
+        //             height: 1.0,
+        //             color: Colors.indigo,
+        //             // child: Text('2'),
+        //           ),
+        //           // Text(
+        //           //   '\nBorn 2003/08/30\n'
+        //           //   'Away 2020/07/25',
+        //           //   style: TextStyle(
+        //           //     fontFamily: 'Source Sans Pro',
+        //           //     fontSize: 10.0,
+        //           //     color: Colors.white,
+        //           //     letterSpacing: 2.5,
+        //           //   ),
+        //           // ),
+        //         ],
+        //       ),
+        //       Column(
+        //         children: <Widget>[
+        //           Container(
+        //             width: 100.0,
+        //             height: 645.0,
+        //             color: Colors.indigo,
+        //           ),
+        //         ],
+        //       ),
+        //     ],
+        //   ),
+        // ),
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("次のページ"),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
       ),
     );
   }
